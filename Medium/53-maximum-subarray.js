47c5b39d61bb467d6224ee1c4ef4141f544206e9
Ashutosh Kumar <kumar.ashutosh920@gmail.com>
2023-09-29 22:47:04 +0530

    save

--- Medium/53-maximum-subarray.js
+++ Medium/53-maximum-subarray.js
@@ -17,14 +17,28 @@ const maxSubArray = (arr) => {
   return sum;
 };
 
+const maxSubArray2 = (arr) => {
+  let max = -Infinity;
+  const len = arr.length;
+  let sum = 0;
+
+  for (let i = 0; i < len; i++) {
+    sum = sum + arr[i];
+    max = Math.max(max, sum);
+    if (sum < 0) sum = 0;
+  }
+
+  return max;
+};
+
 let x, y;
 
-const arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+// const arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
 // const arr = [5,4,-1,7,8];
-// const arr = generateIntegerArray(10000000, -1000000, 1000000);
+const arr = generateIntegerArray(10000000, -1000000, 1000000);
 
 x = performance.now();
-const max = maxSubArray(arr);
+// console.log(maxSubArray(arr));
+console.log(maxSubArray2(arr));
 y = performance.now();
-console.log(max);
 console.log(`Time taken: ${Math.floor(y - x)} ms`);
